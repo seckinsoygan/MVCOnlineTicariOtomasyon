bac135f06ddf5fdd886a365848b766f113f9a03e
Niyazi Seçkin Soygan <seckinsoygann@gmail.com>
2023-07-27 22:40:47 +0300

    Kategori ve kategori ekleme sayfası ve controllerları düzenlendi.

--- MVCOnlineTicariOtomasyon/Controllers/CategoryController.cs
+++ MVCOnlineTicariOtomasyon/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MVCOnlineTicariOtomasyon.Models.DataAccess;
+using MVCOnlineTicariOtomasyon.Models.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,24 @@ namespace MVCOnlineTicariOtomasyon.Controllers
 {
     public class CategoryController : Controller
     {
+        Context context = new Context();
         // GET: Category
         public ActionResult Index()
         {
+            var degerler = context.Categories.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+		{
             return View();
+		}
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            context.Categories.Add(category);
+            context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
